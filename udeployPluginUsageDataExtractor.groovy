7c813b7dad08113e403ce955c7a416abfb8e46f7
shoahua wen <shaohua.wen@gmail.com>
2022-05-25 14:39:52 +0700

    add plugin usage data extractor

---
+++ udeployPluginUsageDataExtractor.groovy
@@ -0,0 +1,42 @@
+def myProjectName = "Holy Proj"
+
+def myApplicationName = args.name
+def myApplicationDesc = args.description
+def myAppTier = "Tier 1"
+
+def myComponents = args.components
+File file = new File("out.txt")
+def plugins = []
+
+myComponents.each { myComponent ->
+  def myComponentName = myComponent.name
+  def myComponentDesc = myComponent.description
+  def myComponentPlugin = getComponentPlugin( myComponent)
+  
+  //println myComponentPlugin
+  def compProcesses = myComponent.processes
+  compProcesses.each{ compProcess ->
+    def compProcessesSteps = compProcess.rootActivity?.children
+    compProcessesSteps.each{ myStep ->
+      //println myStep.type
+      if(myStep.type.equals("plugin"))
+        plugins << myStep.pluginName + ":" + myStep.commandName
+    }
+  }
+  
+}
+ 
+
+
+def getComponentPlugin( component ) {
+    if(component.sourceConfigPluginName.equals("Maven")){
+        return "EC-Maven"
+    } else if (component.sourceConfigPluginName.equals("Artifactory")){
+        return "EC-Artifactory"
+    } else {
+        return "EC-Artifact"
+    }
+}
+
+
+plugins.unique()
