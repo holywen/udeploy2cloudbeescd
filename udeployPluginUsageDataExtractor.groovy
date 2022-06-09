7867a7d7ac507e999e475a55880183086c837b04
holywen <shaohua.wen@gmail.com>
2022-06-09 11:47:19 +0700

    clean up

--- udeployPluginUsageDataExtractor.groovy
+++ udeployPluginUsageDataExtractor.groovy
@@ -1,18 +1,7 @@
-def myProjectName = "Holy Proj"
-
-def myApplicationName = args.name
-def myApplicationDesc = args.description
-def myAppTier = "Tier 1"
-
 def myComponents = args.components
 def plugins = []
 
 myComponents.each { myComponent ->
-  def myComponentName = myComponent.name
-  def myComponentDesc = myComponent.description
-  def myComponentPlugin = getComponentPlugin( myComponent)
-
-  //println myComponentPlugin
   def compProcesses = myComponent.processes
   compProcesses.each{ compProcess ->
     def compProcessesSteps = compProcess.rootActivity?.children
@@ -24,18 +13,5 @@ myComponents.each { myComponent ->
   }
 
 }
-
-
-
-def getComponentPlugin( component ) {
-    if(component.sourceConfigPluginName.equals("Maven")){
-        return "EC-Maven"
-    } else if (component.sourceConfigPluginName.equals("Artifactory")){
-        return "EC-Artifactory"
-    } else {
-        return "EC-Artifact"
-    }
-}
-
 
 plugins.unique().sort()
